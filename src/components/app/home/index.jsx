f5b282b902dd15895bd2b1a29cd4c8c6824df7fb
leonardorpr <leoprocha100@gmail.com>
2019-03-21 22:50:19 -0300

    add (home) component & style

--- src/components/app/home/index.jsx
+++ src/components/app/home/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-
+import { Button } from 'reactstrap';
 import { Header } from 'components/resource';
+import { logo } from 'assets/brands';
 
-// import styles from './styles';
+import styles from './styles';
 
-const Home = () => (
-  <div>
+const Home = ({ className }) => (
+  <div className={className}>
     <Header />
+    <div className="wrapper">
+      <img src={logo} alt="Darth Vader brand" className="logo-body" />
+      <p className="letter-body">StarQuiz</p>
+      <Button className="button-body" color="warning">
+        START
+      </Button>
+    </div>
   </div>
 );
 
-export default Home;
+export default styles(Home);
--- src/components/app/home/styles.js
+++ src/components/app/home/styles.js
@@ -1,6 +1,27 @@
 import styled from 'styled-components';
 
 export default c => styled(c)`
-  .container {
+  .wrapper {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .logo-body {
+    height: 300px;
+    width: 300px;
+  }
+
+  .letter-body {
+    color: #000;
+    font-weight: bolder;
+    font-size: 50px;
+  }
+
+  .button-body {
+    width: 150px;
+    height: 50px;
   }
 `;
